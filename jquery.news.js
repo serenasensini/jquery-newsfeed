3c591103a3ff4d6a498074fdfd83a038e60e46de
Serena Sensini <serena.sensini@gmail.com>
2021-03-16 15:03:10 +0100

    refactor code; fixme add country and sources

--- jquery.news.js
+++ jquery.news.js
@@ -1,59 +1,79 @@
-$(function () {
+var apikey = "276afd811eef446b94420f2377b1e7fe";
 
-    var topic = "";
-    //Italian sources
-    var sources = ["ansa", "la-repubblica", "google-news-it", "abc-news", "ars-technica", "google-news", "associated-press","bleacher-report","business-insider"];
-    //some of the principles English/American sources
-    //sources: ["abc-news", "ars-technica", "google-news", "associated-press","bleacher-report","business-insider"];
-    var max_articles = 3;
+;(function ( $, window, document, undefined ) {
 
-    $.ajax({
-        url      : "https://newsapi.org/v2/top-headlines?apiKey=YOUR_KEY&q="+topic+"&sources="+sources,
-        dataType : 'json',
-        success  : function (data) {
-            $(".news").empty();
-            if (data.status == "ok" && data.totalResults > 0) {
-                console.log("https://newsapi.org/v2/top-headlines?apiKey=YOUR_KEY&q="+topic+"&sources="+sources);
+    // Create the defaults once
+    var pluginName = "news",
+        defaults = {
+            topic: "github",
+            maxResults: "5",
+            listClassName: "item"
+        };
 
-                var counter = 0;
+    // The actual plugin constructor
+    function Plugin( element, options ) {
+        this.element = element;
 
-                $.each(data.articles, function (i, e) {
-                    if(counter < max_articles){
-                         $(".news").append('<li>' + '<a href=\"' + e.url + '\">' + e.title + '</a></li>');
-                         counter++;
+        this.options = $.extend( {}, defaults, options );
+
+        this.init();
+    }
+
+    Plugin.prototype = {
+
+        init: function() {
+
+            this.fetchNews(this.element, this.options);
+
+        },
+
+        fetchNews: function(el, options) {
+            $.ajax({
+                url      : "https://newsapi.org/v2/top-headlines?apiKey=276afd811eef446b94420f2377b1e7fe&q="+options.topic,
+                dataType : 'json',
+                success  : function (data) {
+                    if (data.status === "ok" && data.totalResults > 0) {
+                        console.log(data);
+                        $.each(data.articles, function (i, e) {
+                            var counter = 0;
+
+                            // format date
+                            var d = new Date(e.publishedAt),
+                                month = '' + (d.getMonth() + 1),
+                                day = '' + d.getDate(),
+                                year = d.getFullYear();
+
+                            if (month.length < 2)
+                                month = '0' + month;
+                            if (day.length < 2)
+                                day = '0' + day;
+
+                            let date = [year, month, day].join('-');
+
+                            if(counter < options.maxResults){
+                                let element = ' <div class="card">\n' +
+                                    '  <img src="'+ e.urlToImage +'" alt="News\'image" style="width:100%">\n' +
+                                    '  <div class="container">\n' +
+                                    '  <h4 class="title">'+e.title+'</h4>\n' +
+                                    '  <small>'+ date + ' - ' + e.author +'</small>' +
+                                    '    <p>'+ e.content +'</p>\n' +
+                                    '</div> ';
+                                $(el).append(element);
+                                counter++;
+                            }
+                        });
                     }
-                });
-            }else{
-                $('.news').append('<li>No news were found with these parameters.</li>')
-            }
-        }
-    });
-
-    $('#save_topic').click(function(){
-
-        topic = $("#selectedTopic").val();
-
-        $.ajax({
-            url      : "https://newsapi.org/v2/top-headlines?apiKey=YOUR_KEY&q="+topic+"&sources="+sources,
-            dataType : 'json',
-            success  : function (data) {
-                $(".news").empty();
-                if (data.status == "ok" & data.totalResults > 0) {
-                    console.log("https://newsapi.org/v2/top-headlines?apiKey=YOUR_KEY&q="+topic+"&sources="+sources);
-
-                    counter = 0;
-
-                    $.each(data.articles, function (i, e) {
-                        if(counter < max_articles){
-                             $(".news").append('<li>' + '<a href=\"' + e.url + '\">' + e.title + '</a></li>');
-                             counter++;
-                        }
-                    });
-                }else{
-                    $('.news').append('<li>No news were found with these parameters.</li>')
                 }
+            });
+        }
+    };
+
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new Plugin( this, options ));
             }
         });
- });
+    };
 
-});
+})( jQuery, window, document );
